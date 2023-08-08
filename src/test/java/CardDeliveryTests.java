16925228b29190186b585c68bd967a9e2511803d
Nina <n1ska@outlook.com>
2023-08-08 21:02:53 +0300

    adjusted test

--- src/test/java/CardDeliveryTests.java
+++ src/test/java/CardDeliveryTests.java
@@ -12,18 +12,39 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTests {
     @Test
-    void successTest() {
+    void failPossibilityToInputTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Москва");
-        String testDate = generateDate (3,"dd.MM.yyyy");
-        form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
+        String testDate = generateDate (13,"dd.MM.yyyy");
+        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         form.$("[data-test-id=date] input").setValue(testDate);
         form.$("[data-test-id=name] input").setValue("Иван Петров");
         form.$("[data-test-id=phone] input").setValue("+71234567890");
         form.$("[data-test-id=agreement]").click();
         form.$(".form-field>button").click();
-        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + testDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
+    }
+
+    @Test
+    void successTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Москва");
+        String testDate = generateDate (3,"dd.MM.yyyy");
+        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(testDate);
+        form.$("[data-test-id=name] input").setValue("Иван Петров");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".form-field>button").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + testDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
     }
     private String generateDate(int addDays, String pattern){
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
